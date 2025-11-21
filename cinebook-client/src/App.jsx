6108838789bc0602d0221eb7cf01e0d1100fc684
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-22 00:07:22 +0530

    basic home page created

--- cinebook-client/src/App.jsx
+++ cinebook-client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import Home from "./pages/Home"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
-  return <div>App</div>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="" />
+        <Route />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App
